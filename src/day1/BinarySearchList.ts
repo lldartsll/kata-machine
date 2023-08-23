7c114f9d52b0cfd179bbd941fc6b4edebd2ae479
Rami Naeem <rany.ramy2009@gmail.com>
2023-08-23 17:26:52 +0300

    Binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,15 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-
+    let lo = 0;
+    let hi = haystack.length;
+    do {
+        let mid = Math.floor(lo + (hi - lo) / 2);
+        let v = haystack[mid];
+        if (v === needle) return true;
+        else if (v < needle) {
+            lo = mid + 1;
+        } else {
+            hi = mid;
+        }
+    } while (lo < hi);
+    return false;
 }
