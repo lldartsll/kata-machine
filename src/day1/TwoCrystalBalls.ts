824d81dbe07aa7e55ef0907d5d505977b2f7b0dd
Rami Naeem <rany.ramy2009@gmail.com>
2023-08-25 19:49:50 +0300

    Two crystal balls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,15 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
-
+    const jmpAmoint = Math.floor(Math.sqrt(breaks.length));
+    let i = jmpAmoint;
+    for (; i < breaks.length; i += jmpAmoint) {
+        if (breaks[i]) {
+            break;
+        }
+    }
+    for (let j = i - jmpAmoint; j < breaks.length; j++) {
+        if (breaks[j]) {
+            return j;
+        }
+    }
+    return -1;
 }
